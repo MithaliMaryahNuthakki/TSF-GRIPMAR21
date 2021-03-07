56bf769ba9f00ee8600f9f5027f67d9baf90781b
MithaliMaryahNuthakki <69067041+MithaliMaryahNuthakki@users.noreply.github.com>
2021-03-07 22:42:24 +0530

    Add files via upload

---
+++ Web Development Task 3/index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	  <link rel="icon" href="logo.png" type="image/png" sizes="32x32" border-radius="50%">
+	  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/26ab819582.js"></script>
+    <title>Gift Little Dreams</title>
+	<link rel="stylesheet" type="text/css" href="CSS.css">
+  </head>
+  <body>
+  
+  <?php
+    include 'navbar.html'
+  ?>
+	
+	
+	<div class="jumbotron container">
+	  <h1 class="display-4 title-text">Gift Little Dreams</h1>	 
+	</div>
+	
+	
+  
+	<div class="card quote conatiner">
+		<div class="card-body">
+			<p >"Today's seeds are tomorrow's trees,<br> Start donation see the growth."
+			 <h6 class="text-uppercase font-weight-bold">
+          		<a style="color:black; font-size: 1.2vw" class="badge-warning"href="https://rzp.io/l/q0X11T7b">Donate</a>
+        	</h6>
+      </p>
+		</div>
+	</div>
+	
+	
+	<div id="About" class=" about">
+		<p>India is a home for 20 million orphans.
+			They have their own dreams. What we need to do is to give our helping hands to acheive them.
+			<br>When the slightest of though and help can make a huge change,then why don't we?
+		</p>
+		<img class="landscapeimage"src="pictures/landscape.JPG" >	
+		
+		<p>The children in orphanages don't expect much from us, all they desire for is love and support.
+		  <br>We can bring a smile to their pretty little faces with just spending some time with them.
+		  <br>Apart from this we can also contribute to their happiness without needing to go out of our way by simply donating something useful.
+			<br>One small item that we donate brings about a huge amount of positivity.
+		</p>		
+	</div>
+
+  <?php
+    include 'footer.html'
+  ?>
+  
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Web Development Task 3/todonate.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="logo.png" type="image/png" sizes="32x32" border-radius="50%">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Donation</title>
+	<link rel="stylesheet" type="text/css" href="CSS.css">
+	<style>
+		body{
+		background-color:#FEFFED;
+		}
+	</style>
+  </head>
+  <body>
+  	<?php
+    include 'navbar.html'
+  	?>
+	
+
+	<div class="donation-cards container">
+		<div class="card-deck">
+			<div class="card mb-3 align-item-center" style="max-width: 540px;">
+			  <div class="row no-gutters">
+				<div class="col-md-4">
+				  <img src="pictures/toysdonate.jpg" class="card-img" alt="...">
+				</div>
+				<div class="col-md-8">
+				  <div class="card-body">
+					<h5 class="card-title"><i class="fa fa-futbol-o"></i> Toys </h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					
+				  </div>
+				</div>
+			  </div>
+			</div>
+			<div class="card mb-3" style="max-width: 540px;">
+			  <div class="row no-gutters">
+				<div class="col-md-4">
+				  <img src="pictures/clothesdonate.jpg" class="card-img" alt="...">
+				</div>
+				<div class="col-md-8">
+				  <div class="card-body">
+					<h5 class="card-title"><img src="https://img.icons8.com/material/24/000000/dress-front-view.png"/>Clothes/Acceesories</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					
+				  </div>
+				</div>
+			  </div>
+			</div>
+		</div>
+		<div class="card-deck">
+		<div class="card mb-3 align-item-center" style="max-width: 540px;">
+		  <div class="row no-gutters">
+			<div class="col-md-4">
+			  <img src="pictures/bookdonate.jpg" class="card-img" alt="...">
+			</div>
+			<div class="col-md-8">
+			  <div class="card-body">
+				<h5 class="card-title"><i class="fa fa-book"></i>Books </h5>
+				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				
+			  </div>
+			</div>
+		  </div>
+		</div>
+		<div class="card mb-3" style="max-width: 540px;">
+		  <div class="row no-gutters">
+			<div class="col-md-4">
+			  <img src="pictures/stationarydonate.jpg" class="card-img" alt="...">
+			</div>
+			<div class="col-md-8">
+			  <div class="card-body">
+				<h5 class="card-title"><i class="fa fa-pencil-square-o"> </i> Stationary</h5>
+				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				
+			  </div>
+			</div>
+		  </div>
+		</div>
+	</div>
+	<div class="card-deck">
+		<div class="card mb-3 align-item-center" style="max-width: 540px;">
+		  <div class="row no-gutters">
+			<div class="col-md-4">
+			  <img src="pictures/artsdonate.png" class="card-img" alt="...">
+			</div>
+			<div class="col-md-8">
+			  <div class="card-body">
+				<h5 class="card-title"><i class="fa fa-paint-brush" aria-hidden="true"></i>
+	Arts & Crafts </h5>
+				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				
+			  </div>
+			</div>
+		  </div>
+		</div>
+		<div class="card mb-3" style="max-width: 540px;">
+		  <div class="row no-gutters">
+			<div class="col-md-4">
+			  <img src="pictures/moneydonate.jpg" class="card-img" alt="...">
+			</div>
+			<div class="col-md-8">
+			  <div class="card-body">
+				<h5 class="card-title"><i  class="fa fa-money"></i> Money</h5>
+				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				
+			  </div>
+			</div>
+		  </div>
+		</div>
+	</div>
+	
+	</div>
+
+	<?php
+    include 'footer.html'
+  	?>
+	
+				
+				 
+				 
+				
+  
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
